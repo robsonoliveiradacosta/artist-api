160128a2d886f9cf7c4cad8ac325149c39d8b181
Robson Oliveira da Costa <robsondeveloper@gmail.com>
2021-02-01 09:30:33 -0400

    cria projeto

---
+++ src/main/java/br/gov/mt/pjc/artist/ArtistApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.pjc.artist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/mt/pjc/artist/ArtistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.pjc.artist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
